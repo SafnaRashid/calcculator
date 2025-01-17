6b6fe6c282c94a119e3b53056645db5f6073a916
SafnaRashid <safna3051999@gmail.com>
2025-01-17 14:38:24 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,22 @@ import { useState } from 'react';
 import './App.css'
 useState
 function App() {
-  const [input, setInput] = useState(''); // State to manage the display value
+  const [display, setDisplay] = useState('');
 
-  const handleClick = (value) => {
+  const handleButtonClick = (value) => {
     if (value === 'Clear') {
-      setInput('');
+      setDisplay(''); 
     } else if (value === '=') {
       try {
-        // Evaluate the expression (using eval cautiously)
-        setInput(eval(input).toString());
-      } catch (error) {
-        setInput('Error');
+        setDisplay(eval(display).toString()); 
+      } catch (err) {
+        setDisplay('Error'); 
       }
     } else {
-      setInput((prev) => prev + value); // Append the clicked value
+      setDisplay((prev) => prev + value); 
     }
   };
+
 
 
 
@@ -28,31 +28,31 @@ function App() {
         <div className='text-center mb-4 calc'>
           <h3>Calculator</h3>
           {/* display */}
-          <input className='display' type="text"  placeholder='0' readOnly/>
+          <input className='display'  value={display}  type="text"  placeholder='0' readOnly/>
 
           <div className='row g-2 mt-4 '>
 
-            <div className='col-3'><button  onClick={() => handleClick()} className='btn btn-warning text-black'>7</button></div>
-            <div className='col-3'><button className='btn btn-warning text-black'>8</button></div>
-            <div className='col-3'><button className='btn btn-warning text-black'>9</button></div>
-            <div className='col-3'><button className='btn btn-warning text-black'>/</button></div>
+            <div className='col-3'><button  onClick={() => handleButtonClick('7')} className='btn btn-warning text-black'>7</button></div>
+            <div className='col-3'><button onClick={() => handleButtonClick('8')} className='btn btn-warning text-black'>8</button></div>
+            <div className='col-3'><button onClick={() => handleButtonClick('9')} className='btn btn-warning text-black'>9</button></div>
+            <div className='col-3'><button onClick={() => handleButtonClick('/')} className='btn btn-warning text-black'>/</button></div>
 
-            <div className='col-3'><button className='btn btn-warning text-black'>4</button></div>
-            <div className='col-3'><button className='btn btn-warning text-black'>5</button></div>
-            <div className='col-3'><button className='btn  btn-warning text-black'>6</button></div>
-            <div className='col-3'><button className='btn btn-warning  text-black'>*</button></div>
+            <div className='col-3'><button onClick={() => handleButtonClick('4')} className='btn btn-warning text-black'>4</button></div>
+            <div className='col-3'><button onClick={() => handleButtonClick('5')} className='btn btn-warning text-black'>5</button></div>
+            <div className='col-3'><button onClick={() => handleButtonClick('6')} className='btn  btn-warning text-black'>6</button></div>
+            <div className='col-3'><button onClick={() => handleButtonClick('*')} className='btn btn-warning  text-black'>*</button></div>
 
-            <div className='col-3'><button className='btn btn-warning text-black'>1</button></div>
-            <div className='col-3'><button className='btn btn-warning text-black'>2</button></div>
-            <div className='col-3'><button className='btn  btn-warning text-black'>3</button></div>
-            <div className='col-3'><button className='btn  btn-warning text-black'>-</button></div>
+            <div className='col-3'><button onClick={() => handleButtonClick('1')} className='btn btn-warning text-black'>1</button></div>
+            <div className='col-3'><button onClick={() => handleButtonClick('2')} className='btn btn-warning text-black'>2</button></div>
+            <div className='col-3'><button onClick={() => handleButtonClick('3')} className='btn  btn-warning text-black'>3</button></div>
+            <div className='col-3'><button onClick={() => handleButtonClick('-')} className='btn  btn-warning text-black'>-</button></div>
 
-            <div className='col-3'><button className='btn  btn-warning text-black'>0</button></div>
-            <div className='col-3'><button className='btn btn-warning  text-black'>Clear</button></div>
-            <div className='col-3'><button className='btn btn-warning text-black'>+</button></div>
-            <div className='col-3'><button className='btn btn-warning text-black'>=</button></div>
+            <div className='col-3'><button onClick={() => handleButtonClick('0')} className='btn  btn-warning text-black'>0</button></div>
+            <div className='col-3'><button onClick={() => handleButtonClick('Clear')} className='btn btn-warning  text-black'>Clear</button></div>
+            <div className='col-3'><button onClick={() => handleButtonClick('+')} className='btn btn-warning text-black'>+</button></div>
+            <div className='col-3'><button onClick={() => handleButtonClick('=')} className='btn btn-warning text-black'>=</button></div>
 
-            {/* <div className='col-12 equal'><button className='btn'></button></div> */}
+  
 
 
           </div>  
@@ -61,5 +61,5 @@ function App() {
     </>
   )
 }
-
 export default App
+
